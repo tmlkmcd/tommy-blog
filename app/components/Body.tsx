bea73d0d97da13d8f12ba4f6fce5b7f52ba1116a
Tommy <thomas.mcdevitt@tractable.ai>
2025-03-23 17:01:27 +0000

    Increase opacity of body's background.

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Outlet } from "@remix-run/react";
 
 export const Body: React.FC = () => (
-  <main className="bg-pinkApotheosis-50 bg-opacity-60 px-8 pb-4 text-left lg:px-12">
+  <main className="bg-pinkApotheosis-50 bg-opacity-90 px-8 pb-4 text-left lg:px-12">
     <Outlet />
   </main>
 );
